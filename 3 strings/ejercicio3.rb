242478cd2e15ebfc4b97c47886734885c028e0bf
Rodolfo <rodolfo@MacBook-Pro-de-Rodolfo.local>
2018-06-07 21:22:15 -0400

    Version  final

--- 3 strings/ejercicio3.rb
+++ 3 strings/ejercicio3.rb
@@ -5,4 +5,32 @@
 # 4. Intercalar los elementos: [[1, "lunes"], [2, "martes"], [3, "miércoles"], [9, "jueves"], [12, "viernes"], [31, "sábado"], ["domingo", "domingo"]]
 
 a = [1, 2, 3, 9, 12, 31, 'domingo']
-b = %w[lunes martes miércoles jueves viernes sábado domingo]
+b = %w[lunes martes miercoles jueves viernes sabado domingo]
+
+def concatenar(a,b)
+  return a + b
+end
+
+def concatenar_sin_repetir(x,y)
+  z = x + y
+  return z.uniq
+end
+
+def concatenar_con_repetir(x,y)
+  return x&y
+end
+def intercalar(x,y)
+  return x.zip y
+end
+
+puts "Concatenar a y b"
+print concatenar(a,b)
+puts ""
+puts 'Concatenar sin repetir'
+print concatenar_sin_repetir(a,b)
+puts ""
+puts 'Concatenar con repetir'
+print concatenar_con_repetir(a,b)
+puts ""
+puts 'Intercalar elementos'
+print intercalar(a,b)
